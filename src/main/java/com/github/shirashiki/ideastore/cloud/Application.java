503f8796abd8237d094879831f82b20b8bf3e720
shirashiki <silvio.hirashiki@gmail.com>
2014-08-22 11:32:35 -0400

    First commit

---
+++ src/main/java/com/github/shirashiki/ideastore/cloud/Application.java
@@ -0,0 +1,52 @@
+/*
+ * 
+ * Copyright 2014 Silvio Hirashiki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.github.shirashiki.ideastore.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//Tell Spring to automatically inject any dependencies that are marked in
+//our classes with @Autowired
+@EnableAutoConfiguration
+
+//Tell Spring that this object represents a Configuration for the
+//application
+@Configuration
+
+//Tell Spring to turn on WebMVC (e.g., it should enable the DispatcherServlet
+//so that requests can be routed to our Controllers)
+@EnableWebMvc
+
+//Tell Spring to go and scan our controller package (and all sub packages) to
+//find any Controllers or other components that are part of our application.
+//Any class in this package that is annotated with @Controller is going to be
+//automatically discovered and connected to the DispatcherServlet.
+@ComponentScan
+public class Application {
+
+	// Tell Spring to launch our app!
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		
+
+	}
+
+}
---
+++ src/main/java/com/github/shirashiki/ideastore/cloud/Greeting.java
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright 2014 Silvio Hirashiki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.github.shirashiki.ideastore.cloud;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Object which represents a greeting. It’s annotated with {@literal @}JsonIgnoreProperties 
+ * from the Jackson JSON processing library to indicate that any properties not 
+ * bound in this type should be ignored.
+ * @author silvio hirashiki
+ *
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Greeting {
+    private long id;
+    private String content;
+
+    /**
+     * Empty constructor added to support JSON to object conversion using jackson
+     */
+    public Greeting(){
+    }
+    
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
